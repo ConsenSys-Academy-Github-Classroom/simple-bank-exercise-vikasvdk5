5b49b6f42a6d37135173405cdb45052a354c63b9
Vikas Kundalpady <vikas.kundz@gmail.com>
2021-11-29 16:22:54 +0800

    contract completed with successfull test case execution

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -14,29 +14,32 @@ contract SimpleBank {
     
     // Fill in the visibility keyword. 
     // Hint: We want to protect our users balance from other contracts
-    mapping (address => uint) balances ;
+    mapping (address => uint) public balances ;
     
     // Fill in the visibility keyword
     // Hint: We want to create a getter function and allow contracts to be able
     //       to see if a user is enrolled.
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     // Let's make sure everyone knows who owns the bank, yes, fill in the
     // appropriate visilibility keyword
-    address owner = msg.sender;
+    address public owner = msg.sender;
     
     /* Events - publicize actions to external listeners
      */
     
     // Add an argument for this event, an accountAddress
-    event LogEnrolled();
+    event LogEnrolled(address  accountAddress);
 
     // Add 2 arguments for this event, an accountAddress and an amount
-    event LogDepositMade();
+    event LogDepositMade(address  accountAddress, uint amount);
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
-    event LogWithdrawal();
+    event LogWithdrawal(address  accountAddress, uint withdrawAmount, uint newBalance);
+
+    // Event to enit when account is overdrawn
+    event LogNotEnoughFundsToWithdraw(address  accountAddress, uint amount);
 
     /* Functions
      */
@@ -56,6 +59,7 @@ contract SimpleBank {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -63,21 +67,28 @@ contract SimpleBank {
     // Emit the appropriate event
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
+      enrolled[msg.sender] = true;
+      emit LogEnrolled(msg.sender);
+      return  enrolled[msg.sender];
     }
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
-
+        if(enrolled[msg.sender]) {
       // 3. Add the amount to the user's balance. Hint: the amount can be
       //    accessed from of the global variable `msg`
-
+          uint curBalance = balances[msg.sender];
+          curBalance = curBalance + msg.value; 
+          balances[msg.sender] = curBalance;
       // 4. Emit the appropriate event associated with this function
-
+          emit LogDepositMade(msg.sender,curBalance);
       // 5. return the balance of sndr of this transaction
+          return curBalance;
+      }
     }
 
     /// @notice Withdraw ether from bank
@@ -89,12 +100,20 @@ contract SimpleBank {
       // Subtract the amount from the sender's balance, and try to send that amount of ether
       // to the user attempting to withdraw. 
       // return the user's balance.
-
+        uint curBalance = balances[msg.sender];   
       // 1. Use a require expression to guard/ensure sender has enough funds
-
+        if(curBalance>=withdrawAmount){
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
-
+          uint newBalance = curBalance - withdrawAmount;
+          balances[msg.sender] = newBalance;
       // 3. Emit the appropriate event for this message
+          emit LogWithdrawal(msg.sender, withdrawAmount, newBalance);
+          return newBalance;
+        } else {
+          require(curBalance>=withdrawAmount , "Balance too low. Not enough funds.");
+          emit LogNotEnoughFundsToWithdraw(msg.sender,curBalance);
+          revert();
+        }
     }
 }
